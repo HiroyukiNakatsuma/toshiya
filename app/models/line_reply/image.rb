1269e5dacb36ddf870b5d02e617a926d0fa1efd1
nakatsuma <>
2018-07-01 22:51:51 +0900

    画像の置き場所をpublic配下に変更

--- app/models/line_reply/image.rb
+++ app/models/line_reply/image.rb
@@ -1,7 +1,7 @@
 class LineReply::Image
 
-  GIVE_UP_IMAGE_PATH = "#{Settings.url.domain}/assets/lines/reply_images/give_up_1024_1024.jpg"
-  GIVE_UP_SMALL_IMAGE_PATH = "#{Settings.url.domain}/assets/lines/reply_images/give_up_240_240.jpg"
+  GIVE_UP_IMAGE_PATH = "#{Settings.url.domain}/images/give_up_1024_1024.jpg"
+  GIVE_UP_SMALL_IMAGE_PATH = "#{Settings.url.domain}/images/give_up_240_240.jpg"
 
   class << self
     def create(original_image_url, preview_image_url)
