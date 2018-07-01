072646edda39eae8cfda6ac4fb084305bad0fc91
nakatsuma <>
2018-07-01 21:13:16 +0900

    モデルのクラス名を変更

--- app/models/line_reply/image.rb
+++ app/models/line_reply/image.rb
@@ -1,4 +1,4 @@
-class Image
+class LineReply::Image
 
   GIVE_UP_IMAGE_PATH = '/assets/lines/reply_images/give_up_1024_1024.jpg'
   GIVE_UP_SMALL_IMAGE_PATH = '/assets/lines/reply_images/give_up_240_240.jpg'
